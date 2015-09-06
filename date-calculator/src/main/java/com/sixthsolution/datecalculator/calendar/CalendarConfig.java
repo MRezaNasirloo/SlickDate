07ea9d0205afa2f31b54d3ba6ea4af9cadaf12d7
M. Reza Nasirloo <M.Reza.Nasirloo@gmai.com>
2015-09-06 11:09:30 +0430

    Adds Builder for CalenderConfig.

--- date-calculator/src/main/java/com/sixthsolution/datecalculator/calendar/CalendarConfig.java
+++ date-calculator/src/main/java/com/sixthsolution/datecalculator/calendar/CalendarConfig.java
@@ -1,48 +1,79 @@
 package com.sixthsolution.datecalculator.calendar;
 
-import java.util.ArrayList;
 import org.joda.time.DateTimeConstants;
+
+import java.util.ArrayList;
 
 public class CalendarConfig {
 
-  public static final int SATURDAY = DateTimeConstants.SATURDAY;
-  public static final int SUNDAY = DateTimeConstants.SUNDAY;
-  public static final int MONDAY = DateTimeConstants.MONDAY;
+    public static final int SATURDAY = DateTimeConstants.SATURDAY;
+    public static final int SUNDAY = DateTimeConstants.SUNDAY;
+    public static final int MONDAY = DateTimeConstants.MONDAY;
 
-  public enum Chronology {
-    ISO, JALALI, ISLAMIC
-  }
+    public enum Chronology {
+        ISO, JALALI, ISLAMIC
+    }
 
-  public ArrayList<Chronology> chronologies = new ArrayList<>();
+    public ArrayList<Chronology> chronologies = new ArrayList<>();
 
-  public int timeZoneHour = 0;
-  public int timeZoneMinute = 0;
-  public boolean hasTimeZone = false;
-  public int firstDayOfWeek = MONDAY;
-  public String locale = "en";
+    public int timeZoneHour = 0;
+    public int timeZoneMinute = 0;
+    public boolean hasTimeZone = false;
+    public int firstDayOfWeek = MONDAY;
+    public String locale = "en";
 
-  public CalendarConfig() {
-    chronologies.add(Chronology.ISO);
-  }
+    private CalendarConfig() {
+        chronologies.add(Chronology.ISO);
+    }
 
-  public void addChronology(Chronology chronology) {
+    private CalendarConfig(ConfigBuilder builder) {
+        timeZoneHour = builder.timeZoneHour;
+        timeZoneMinute = builder.timeZoneMinute;
+        hasTimeZone = builder.hasTimeZone;
+        firstDayOfWeek = builder.firstDayOfWeek;
+        locale = builder.locale;
+        chronologies.add(Chronology.ISO);
+    }
 
-  }
+    public void addChronology(Chronology chronology) {
 
-  public void addChronolgy(Chronology... chronology) {
+    }
 
-  }
+    public void addChronolgy(Chronology... chronology) {
 
-  public void setTimeZone(int hour, int minute) {
-    timeZoneHour = hour;
-    timeZoneMinute = minute;
-    hasTimeZone = true;
-  }
+    }
 
-  public void setFirstDayOfWeek(int firstDayOfWeek) {
-  }
+    /**
+     * Builder for creating {@link CalendarConfig} object.
+     */
+    public static class ConfigBuilder {
 
-  public void setLocale(String locale) {
+        public int timeZoneHour = 0;
+        public int timeZoneMinute = 0;
+        public boolean hasTimeZone = false;
+        public int firstDayOfWeek = MONDAY;
+        public String locale = "en";
 
-  }
+        public ConfigBuilder setTimeZone(int hour, int minute) {
+            this.timeZoneHour = hour;
+            this.timeZoneMinute = minute;
+            hasTimeZone = true;
+            return this;
+        }
+
+        public ConfigBuilder setFirstDayOfWeek(int firstDayOfWeek) {
+            this.firstDayOfWeek = firstDayOfWeek;
+            return this;
+        }
+
+        public ConfigBuilder setLocale(String locale) {
+            this.locale = locale;
+            return this;
+        }
+
+        public CalendarConfig build() {
+            return new CalendarConfig(this);
+        }
+
+    }
 }
